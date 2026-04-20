39602b974aa04cf6ff6150466b39eb35bbe410c5
rohit kumar <rohitpsingh2035@gmail.com>
2026-04-20 17:08:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Hero from "./components/Hero";
+import { CardContainer } from "./components/CardContainer";
+import "./styles/style.css";
+import "./App.css";
+import Footer from "./components/Footer";
+import Info from "./components/Info";
+
+export default function App() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="layout">
+        <Sidebar open={open} setOpen={setOpen} />
+
+        <div className={`main ${open ? "shift" : ""}`}>
+          <Hero />
+
+          <CardContainer open={open} />
+          
+        </div>
+      </div>
+      <Info />
+      <Footer />
+    </>
+  );
+}
